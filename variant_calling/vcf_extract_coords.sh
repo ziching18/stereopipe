01bed590afa9afd84bb8084a9124f85f74bee54c
ziching18 <zichingtan18@gmail.com>
2025-02-10 13:54:37 +0800

    no overwrite edit

--- variant_calling/vcf_extract_coords.sh
+++ variant_calling/vcf_extract_coords.sh
@@ -1,8 +1,8 @@
 TUM_ID=$1
 muttype=$2;
 path=$(echo /stereoseq/all_samples/mutations/$TUM_ID);
-touch $path/coords/$TUM_ID.somatic.$muttype.coords.txt;
 
 for file in $(ls $path/bams/$muttype/*.$muttype.bam); do
+    touch $path/coords/$TUM_ID.somatic.$muttype.coords.txt;
     samtools view $file | awk '{print $1,$(NF-1),$NF}' >> $path/coords/$TUM_ID.somatic.$muttype.coords.txt;
 done
--- variant_calling/vcf_filter_variants.py
+++ variant_calling/vcf_filter_variants.py
@@ -7,16 +7,18 @@ def main(sample, skiprows, in_dir, out_dir, force):
     #sr = int(skiprows) ## number of rows to skip for vcf
     extract = True
 
-    if os.path.exists("{0}/{1}.somatic.all.tsv".format(out_dir, d)):
+    if os.path.exists("{0}/{1}.somatic.funcotated.tsv".format(out_dir, d)):
         print("Variants already extracted sis\nUse --force to replace existing file")
         if not force:
             extract = False
-        #df = pd.read_csv("{0}{1}/{1}.somatic.funcotated.tsv".format(out_dir, d), sep="\t")
+        else:
+            extract = True
         #if force:
         #    df.to_csv("{0}{1}/{1}.somatic.funcotated.tsv".format(out_dir, d), sep="\t")
     
     if extract:
         df = pd.read_csv("{0}/{1}.somatic.funcotated.vcf".format(in_dir, d), skiprows=skiprows-1, sep="\t")
+        df.to_csv("{0}/{1}.somatic.funcotated.tsv".format(in_dir, d))
         seqs = [] 
         for i in range(len(df)):
             seqs.append(df.iloc[i]["INFO"].split("|")[21])
@@ -29,11 +31,13 @@ def main(sample, skiprows, in_dir, out_dir, force):
         df["GENE"] = allgenes
         col = df.pop("GENE")
         df.insert(5, "GENE", col)
-        df.to_csv("{0}/{1}.somatic.all.tsv".format(out_dir, d), sep="\t")
+        df.to_csv("{0}/{1}.somatic.all.tsv".format(out_dir, d), sep="\t", header=False)
 
         driver_genes = ["TP53","PIK3CA","GATA3","MAP3K1","KMT2C","PTEN","CBFB","CDH1","AKT1","NF1"]
         for gene in driver_genes:
-            df[df["GENE"]==gene].to_csv("{0}/{1}.somatic.{2}.tsv".format(out_dir, d, gene), sep="\t", header=False) ## ga
+            dfgene = df[df["GENE"]==gene]
+            if len(dfgene) > 0:
+                dfgene.to_csv("{0}/{1}.somatic.{2}.tsv".format(out_dir, d, gene), sep="\t", header=False) ## ga
 
         ## extract mutations
         changes = [
