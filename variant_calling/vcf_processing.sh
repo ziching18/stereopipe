ea7fd95ae2523ac49766d8701264153f8b571abf
ziching18 <zichingtan18@gmail.com>
2025-02-10 14:08:32 +0800

    edit path

--- variant_calling/vcf_processing.sh
+++ variant_calling/vcf_processing.sh
@@ -22,7 +22,7 @@ for muttype in all CT GA AT TCA YTCA RTCA TP53 PIK3CA GATA3 MAP3K1 KMT2C PTEN CB
 done;
 
 python3 /stereoseq/code/stereopipe/variant_calling/vcf_round_counts.py -s ${TUM_ID} \
---bin_size 100 --dir "stereoseq/all_samples/mutations/${TUM_ID}";
+--bin_size 100 --dir "/stereoseq/all_samples/mutations/${TUM_ID}";
 
 python3 /stereoseq/code/stereopipe/variant_calling/vcf_normlog.py -s ${TUM_ID} \
---bin_size 100 --dir "stereoseq/all_samples/mutations/${TUM_ID}";
+--bin_size 100 --dir "/stereoseq/all_samples/mutations/${TUM_ID}";
