5e8a1442491bca9e65ad6ba8b2e0d89ceed2bf66
ziching18 <zichingtan18@gmail.com>
2025-02-11 17:23:06 +0800

    update code from hpc

--- neoantigen/neo_pvacseq_prediction.sh
+++ neoantigen/neo_pvacseq_prediction.sh
@@ -1,17 +1,23 @@
-## NEOANTIGEN PREDICTION
+# NEOANTIGEN PREDICTION
+# while read line; do 
+#	slx=$(echo $line | awk '{print $1}'); 
+#	id=$(echo $line | awk '{print $2}'); 
+#	hla=$(grep $id /stereoseq/neoantigens/data/neoantigens_hla_alleles.txt | awk '{print}');
+#	sudo docker exec neo bash -c bash /stereoseq/code/pvacseq_neoantigens_prediction.sh $slx $id $hla; 
+#done < /stereoseq/code/samples.txt
 
 TUM_ID=$1;
 HLA=$2; 
 
-#echo "running..." ${TUM_ID} ${HLA};
+echo "running..." ${TUM_ID} ${HLA};
 pvacseq run \
-/vcf/${TUM_ID}/${TUM_ID}.somatic.funcotated.vep.vcf.gz \
+/vcf/${TUM_ID}.somatic.funcotated.vep.vcf.gz \
 ${TUM_ID} \
 ${HLA} \
 MHCflurry MHCflurryEL NNalign NetMHC NetMHCIIpan NetMHCIIpanEL NetMHCpan NetMHCpanEL \
-/pvactools_stereoseq_output \
+/output \
 -e1 8,9,10,11 \
 -e2 15 \
--p /vcf/${TUM_ID}/${TUM_ID}.phased.vep.vcf.gz \
+-p /vcf/${TUM_ID}.phased.vep.vcf.gz \
 --iedb-install-directory /opt/iedb ;
-#echo "finished ${TUM_ID}
+echo "finished ${TUM_ID}
--- neoantigen/neo_run_pvacseq.sh
+++ neoantigen/neo_run_pvacseq.sh
@@ -1,15 +1,16 @@
 TUM_ID=$1;
 
-mkdir /stereoseq/neoantigens/output_${TUM_ID};
+#mkdir /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/code/out/output_${TUM_ID};
 
-hla=$(grep ${TUM_ID} /stereoseq/neoantigens/data/neoantigens_hla_alleles.txt | awk '{print $NF}');
-sudo docker rm --force ${TUM_ID}_neo;
+hla=$(grep ${TUM_ID} data/all_neoantigens_hla_alleles.txt | awk '{print $NF}');
+echo ${TUM_ID} $hla;
+#sudo docker rm --force ${TUM_ID}_neo;
 sudo docker run \
 --name ${TUM_ID}_neo \
--v /stereoseq/all_samples/vcf:/vcf \
--v /stereoseq/neoantigens/data:/pvactools_stereoseq_data \
--v /stereoseq/neoantigens/output_${TUM_ID}:/pvactools_stereoseq_output \
--v /home/ubuntu/tools/VEP_plugins:/VEP_plugins \
--v /stereoseq/code:/code \
+-v /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/vcf:/vcf \
+-v /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/data:/data \
+-v /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/out/output_${TUM_ID}:/output \
+-v /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/VEP_plugins:/VEP_plugins \
+-v /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/code:/code \
 -dit griffithlab/pvactools ;
-sudo docker exec ${TUM_ID}_neo bash -c "bash /code/pvacseq_neoantigens_prediction.sh ${TUM_ID} $hla"; 
+sudo docker exec ${TUM_ID}_neo bash -c "bash /mnt/c/Users/staffgenomics/wsl_workdir/ziching/neo/code/neo_pvacseq_prediction.sh ${TUM_ID} $hla"; 
