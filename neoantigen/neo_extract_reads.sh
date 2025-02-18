9eeee39d59bb119cbf2542c368e7b115fa321e78
ziching18 <zichingtan18@gmail.com>
2025-02-18 21:27:51 +0800

    update path

--- neoantigen/neo_extract_reads.sh
+++ neoantigen/neo_extract_reads.sh
@@ -7,8 +7,8 @@ linenumber=$(wc -l $path/logs/$TUM_ID.neoantigens.$muttype.log.txt | awk '{print
 
 echo chr pos ref alt gene chr.pos.ref.alt.gene count > $path/counts/$TUM_ID.neoantigens.vcf.transcript.$muttype.gene.counts.tsv;
 while read line; do
-    rm $path/$muttype.subset.bam;
-    rm $path/$muttype.skrip.js;
+    rm $path/bams/$muttype.subset.bam;
+    rm $path/bams/$muttype.skrip.js;
     chr=$(echo $line | awk '{print $2}');
     pos=$(echo $line | awk '{print $3}');
     fullpos=$(echo $chr\:$(($pos-500))\-$(($pos+500)));
@@ -17,13 +17,13 @@ while read line; do
     alt=$(echo $line | awk '{print $6}' | cut -c1-1);
     #gene=$(echo $line | awk '{print $8}' | cut -d '[' -f 2 | cut -d '|' -f 1);
     gene=$(echo $line | awk '{print $7}');
-    echo "final String contig= \"$chr\";" >> $path/$muttype.skrip.js;
-    echo "final int mutpos = $pos;" >> $path/$muttype.skrip.js;
-    echo "final char mutbase='$alt';" >> $path/$muttype.skrip.js;
+    echo "final String contig= \"$chr\";" >> $path/bams/$muttype.skrip.js;
+    echo "final int mutpos = $pos;" >> $path/bams/$muttype.skrip.js;
+    echo "final char mutbase='$alt';" >> $path/bams/$muttype.skrip.js;
     cat /stereoseq/all_samples/neoantigens/scriptbody.js >> $path/bams/$muttype.skrip.js;
-    samtools view -b /stereoseq/all_samples/bams/$TUM_ID/$TUM_ID.recal.RGA.bam $fullpos > $path/$muttype.subset.bam;
+    samtools view -b /stereoseq/all_samples/bams/$TUM_ID/$TUM_ID.recal.RGA.bam $fullpos > $path/bams/$muttype.subset.bam;
     java -jar ~/tools/jvarkit/dist/jvarkit.jar samjdk \
-    -f $path/$muttype.skrip.js $path/$muttype.subset.bam \
+    -f $path/bams/$muttype.skrip.js $path/bams/$muttype.subset.bam \
     -o $path/bams/$muttype/$TUM_ID.neoantigens.chr$chr.$pos.$muttype.bam --samoutputformat BAM;
     count=$(samtools view $path/bams/$muttype/$TUM_ID.neoantigens.chr$chr.$pos.$muttype.bam | wc -l | awk '{print $1}');
     echo $chr $pos $ref $alt $gene chr$chr.$pos.$ref.$alt.$gene $count >> $path/counts/$TUM_ID.neoantigens.vcf.transcript.$muttype.gene.counts.tsv; # writing counts to file
