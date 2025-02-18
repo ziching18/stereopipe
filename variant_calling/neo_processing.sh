de68818d3ed8706a5810b8c2cae5e81485d8698b
ziching18 <zichingtan18@gmail.com>
2025-02-18 20:55:23 +0800

    update neo processing

--- variant_calling/neo_processing.sh
+++ variant_calling/neo_processing.sh
@@ -1,5 +1,4 @@
 TUM_ID=$1;
-rows=$(grep "#" /stereoseq/all_samples/vcf/${TUM_ID}/${TUM_ID}.somatic.funcotated.vcf | wc -l);
 
 bash /stereoseq/code/stereopipe/neoantigens/neo_extract_vcf.sh ${TUM_ID};
 
@@ -7,16 +6,16 @@ python3 /stereoseq/code/stereopipe/variant_calling/neo_filter_variants.py -s ${T
 --in_dir "/stereoseq/all_samples/neoantigens/${TUM_ID}" \
 --out_dir "/stereoseq/all_samples/neoantigens/${TUM_ID}/variants";
 
-for muttype in all CT GA AT TCA YTCA RTCA TP53 PIK3CA GATA3 MAP3K1 KMT2C PTEN CBFB CDH1 AKT1 NF1; do
-    if [ ! -f /stereoseq/all_samples/mutations/$TUM_ID/logs/$TUM_ID.$muttype.extract_reads.success ]; then
+for muttype in all CT GA AT TCA YTCA RTCA; do
+    if [ ! -f /stereoseq/all_samples/neoantigens/$TUM_ID/logs/$TUM_ID.$muttype.extract_reads.success ]; then
         bash /stereoseq/code/stereopipe/variant_calling/neo_extract_reads.sh ${TUM_ID} ${muttype};
     else
         echo "Reads already extracted sis";
     fi;
 done;
 
-for muttype in all CT GA AT TCA YTCA RTCA TP53 PIK3CA GATA3 MAP3K1 KMT2C PTEN CBFB CDH1 AKT1 NF1; do
-    if [ ! -f /stereoseq/all_samples/mutations/$TUM_ID/coords/$TUM_ID.somatic.$muttype.coords.txt ]; then
+for muttype in all CT GA AT TCA YTCA RTCA; do
+    if [ ! -f /stereoseq/all_samples/neoantigens/$TUM_ID/coords/$TUM_ID.somatic.$muttype.coords.txt ]; then
         bash /stereoseq/code/stereopipe/variant_calling/neo_extract_coords.sh ${TUM_ID} ${muttype};
     else
         echo "Coords already extracted sis";
@@ -24,7 +23,7 @@ for muttype in all CT GA AT TCA YTCA RTCA TP53 PIK3CA GATA3 MAP3K1 KMT2C PTEN CB
 done;
 
 python3 /stereoseq/code/stereopipe/variant_calling/neo_round_counts.py -s ${TUM_ID} \
---bin_size 100 --dir "/stereoseq/all_samples/mutations/${TUM_ID}";
+--bin_size 100 --dir "/stereoseq/all_samples/neoantigens/${TUM_ID}";
 
 python3 /stereoseq/code/stereopipe/variant_calling/neo_normlog.py -s ${TUM_ID} \
---bin_size 100 --dir "/stereoseq/all_samples/mutations/${TUM_ID}";
+--bin_size 100 --dir "/stereoseq/all_samples/neoantigens/${TUM_ID}";
