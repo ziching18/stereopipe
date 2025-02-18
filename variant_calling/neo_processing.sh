e7bed3cb2dec616ca6c3259991df5713464e9dbd
ziching18 <zichingtan18@gmail.com>
2025-02-18 20:59:31 +0800

    update path

--- variant_calling/neo_processing.sh
+++ variant_calling/neo_processing.sh
@@ -1,14 +1,14 @@
 TUM_ID=$1;
 
-bash /stereoseq/code/stereopipe/variant_calling/neo_extract_vcf.sh ${TUM_ID};
+bash /stereoseq/code/stereopipe/neoantigen/neo_extract_vcf.sh ${TUM_ID};
 
-python3 /stereoseq/code/stereopipe/variant_calling/neo_filter_variants.py -s ${TUM_ID} \
+python3 /stereoseq/code/stereopipe/neoantigen/neo_filter_variants.py -s ${TUM_ID} \
 --in_dir "/stereoseq/all_samples/neoantigens/${TUM_ID}" \
 --out_dir "/stereoseq/all_samples/neoantigens/${TUM_ID}/variants";
 
 for muttype in all CT GA AT TCA YTCA RTCA; do
     if [ ! -f /stereoseq/all_samples/neoantigens/$TUM_ID/logs/$TUM_ID.$muttype.extract_reads.success ]; then
-        bash /stereoseq/code/stereopipe/variant_calling/neo_extract_reads.sh ${TUM_ID} ${muttype};
+        bash /stereoseq/code/stereopipe/neoantigen/neo_extract_reads.sh ${TUM_ID} ${muttype};
     else
         echo "Reads already extracted sis";
     fi;
@@ -16,14 +16,14 @@ done;
 
 for muttype in all CT GA AT TCA YTCA RTCA; do
     if [ ! -f /stereoseq/all_samples/neoantigens/$TUM_ID/coords/$TUM_ID.somatic.$muttype.coords.txt ]; then
-        bash /stereoseq/code/stereopipe/variant_calling/neo_extract_coords.sh ${TUM_ID} ${muttype};
+        bash /stereoseq/code/stereopipe/neoantigen/neo_extract_coords.sh ${TUM_ID} ${muttype};
     else
         echo "Coords already extracted sis";
     fi;
 done;
 
-python3 /stereoseq/code/stereopipe/variant_calling/neo_round_counts.py -s ${TUM_ID} \
+python3 /stereoseq/code/stereopipe/neoantigen/neo_round_counts.py -s ${TUM_ID} \
 --bin_size 100 --dir "/stereoseq/all_samples/neoantigens/${TUM_ID}";
 
-python3 /stereoseq/code/stereopipe/variant_calling/neo_normlog.py -s ${TUM_ID} \
+python3 /stereoseq/code/stereopipe/neoantigen/neo_normlog.py -s ${TUM_ID} \
 --bin_size 100 --dir "/stereoseq/all_samples/neoantigens/${TUM_ID}";
