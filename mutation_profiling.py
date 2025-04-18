0416e97cdde7479aac1634578d344a70d91ddc36
ziching18 <zichingtan18@gmail.com>
2025-04-18 13:51:37 +0800

    add args

--- mutation_profiling.py
+++ mutation_profiling.py
@@ -1,6 +1,6 @@
 from SigProfilerAssignment import Analyzer as Analyze
 
-def main(samples, output):
+def main(samples, output, input_type, context_type):
 
     ## to exclude
     exclude_signature_subgroups = [
@@ -15,19 +15,20 @@ def main(samples, output):
         "Lymphoid_signatures"
     ]
 
-    Analyze.cosmic_fit(samples, output, input_type="vcf", context_type="96",
+    Analyze.cosmic_fit(samples, output, input_type=input_type, context_type=context_type,
                     collapse_to_SBS96=True, cosmic_version=3.4, exome=True,
                     genome_build="GRCh38", signature_database=None,
                     exclude_signature_subgroups=exclude_signature_subgroups, 
                     export_probabilities=True,
                     export_probabilities_per_mutation=True, make_plots=True,
-                    sample_reconstruction_plots="png", verbose=False)
+                    sample_reconstruction_plots="pdf", verbose=False)
     
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--samples")
     parser.add_argument("-o", "--output")
-    #parser.add_argument("--force", action="store_true")
+    parser.add_argument("--input_type")
+    parser.add_argument("--context_type")
     args = parser.parse_args()
-    main(args.samples, args.output)
+    main(args.samples, args.output, args.input_type, args.context_type)
