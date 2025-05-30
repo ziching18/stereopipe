87710245f80aea89cd684e10ac7c20bde731e001
ziching18 <zichingtan18@gmail.com>
2025-05-30 10:58:12 +0800

    include pvacseq information in extracted vcf

--- neoantigen/neo_extract_vcf.sh
+++ neoantigen/neo_extract_vcf.sh
@@ -1,13 +1,16 @@
 TUM_ID=$1;
 
-> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
-lines1=$(wc -l < /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.filtered.tsv)
+lines1=$(wc -l < /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID_stereo_TUM.filtered.tsv)
 lines2=$(($lines1 - 1))
+echo $(head -1 /stereoseq/all_samples/neoantigens/$TUM_ID/${TUM_ID}_stereo_TUM.filtered.tsv) \
+$(grep "#" /stereoseq/all_samples/vcf/$TUM_ID/$TUM_ID.somatic.filtered7.funcotated.vcf | tail -1) \
+> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
+
 while read line; do
 	chr=$(echo $line | awk '{print $1}');
 	var_stop=$(echo $line | awk '{print $3}');
 	transcript=$(echo $line | awk '{print $6}');
 	peptide=$(echo $line | awk '{print $21}');
-	awk -v stop="$var_stop" '$2 ~ stop {print}' /stereoseq/all_samples/vcf/$TUM_ID/$TUM_ID.somatic.funcotated.vcf \
-    >> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
-done < <(tail -$lines2 /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.filtered.tsv)
+	vcfLine=$(awk -v stop="$var_stop" '$2 ~ stop {print}' /stereoseq/all_samples/vcf/$TUM_ID/$TUM_ID.somatic.filtered7.funcotated.vcf);
+    echo $vcfLine $line >> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
+done < <(tail -$lines2 /stereoseq/all_samples/neoantigens/$TUM_ID/${TUM_ID}_stereo_TUM.filtered.tsv)
