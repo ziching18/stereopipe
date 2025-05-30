8399e651236aec6426f7d2bd4d316d479ea627e1
ziching18 <zichingtan18@gmail.com>
2025-05-30 15:04:25 +0800

    revert to 78b9a1b

--- neoantigen/neo_extract_vcf.sh
+++ neoantigen/neo_extract_vcf.sh
@@ -1,16 +1,13 @@
 TUM_ID=$1;
 
-lines1=$(wc -l < /stereoseq/all_samples/neoantigens/$TUM_ID/${TUM_ID}_stereo_TUM.filtered.tsv);
-lines2=$(($lines1 - 1));
-head1=$(head -1 /stereoseq/all_samples/neoantigens/$TUM_ID/${TUM_ID}_stereo_TUM.filtered.tsv);
-head2=$(grep "#" /stereoseq/all_samples/vcf/$TUM_ID/$TUM_ID.somatic.filtered7.funcotated.vcf | tail -1);
-echo $head1 $head2 > /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
-
+> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
+lines1=$(wc -l < /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.filtered.tsv)
+lines2=$(($lines1 - 1))
 while read line; do
 	chr=$(echo $line | awk '{print $1}');
 	var_stop=$(echo $line | awk '{print $3}');
 	transcript=$(echo $line | awk '{print $6}');
 	peptide=$(echo $line | awk '{print $21}');
-	vcfLine=$(awk -v stop="$var_stop" '$2 ~ stop {print}' /stereoseq/all_samples/vcf/$TUM_ID/$TUM_ID.somatic.filtered7.funcotated.vcf);
-    echo $vcfLine $line >> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
-done < <(tail -$lines2 /stereoseq/all_samples/neoantigens/$TUM_ID/${TUM_ID}_stereo_TUM.filtered.tsv)
+	awk -v stop="$var_stop" '$2 ~ stop {print}' /stereoseq/all_samples/vcf/$TUM_ID/$TUM_ID.somatic.funcotated.vcf \
+    >> /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.all.vcf.tsv;
+done < <(tail -$lines2 /stereoseq/all_samples/neoantigens/$TUM_ID/$TUM_ID.neoantigens.filtered.tsv)
