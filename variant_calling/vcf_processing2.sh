7940489d9e252d9dc398ed4d6f2a17226dd74d48
ziching18 <zichingtan18@gmail.com>
2025-06-16 13:02:02 +0800

    add force option to bash script

--- variant_calling/vcf_processing2.sh
+++ variant_calling/vcf_processing2.sh
@@ -1,4 +1,5 @@
 TUM_ID=$1;
+FORCE=$2;
 # rows=$(grep "#" /stereoseq/all_samples/vcf/${TUM_ID}/${TUM_ID}.somatic.funcotated.vcf | wc -l);
 
 # python3 /stereoseq/code/stereopipe/variant_calling/vcf_filter_variants.py -s ${TUM_ID} \
@@ -10,4 +11,4 @@ TUM_ID=$1;
 bash /stereoseq/code/stereopipe/variant_calling/vcf_extract_coords.sh ${TUM_ID} filtered;
 
 python3 /stereoseq/code/stereopipe/variant_calling/vcf_raw_round.py -s ${TUM_ID} \
---bin_size 100 --dir "/stereoseq/all_samples/mutations/${TUM_ID}" --muttype filtered;
+--bin_size 100 --dir "/stereoseq/all_samples/mutations/${TUM_ID}" --muttype filtered $FORCE;
