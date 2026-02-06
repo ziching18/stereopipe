5dda8025e49be8e41080ad6ed5026b2ef1c56ddc
ziching18 <zichingtan18@gmail.com>
2026-02-06 19:53:17 +0800

    update path

--- fusion_neoantigen/fn_processing.sh
+++ fusion_neoantigen/fn_processing.sh
@@ -1,8 +1,8 @@
 TUM_ID=$1;
 
-bash stereopipe/variant_processing/neoantigen/fn_extract_reads.sh ${TUM_ID} fusion;
+bash stereopipe/variant_processing/neoantigen/fusion/fn_extract_reads.sh ${TUM_ID} fusion;
 
-bash stereopipe/variant_processing/neoantigen/fn_extract_coords.sh ${TUM_ID} fusion;
+bash stereopipe/variant_processing/neoantigen/fusion/fn_extract_coords.sh ${TUM_ID} fusion;
 
-python3 stereopipe/variant_processing/neoantigen/fn_raw_round.py -s ${TUM_ID} \
---bin_size 100 --dir "/stereoseq/all_samples/neoantigens/${TUM_ID}" --muttype fusion;
+python3 stereopipe/variant_processing/neoantigen/fusion/fn_raw_round.py -s ${TUM_ID} \
+--bin_size 100 --dir "/stereoseq/all_samples/neoantigens/fusion/${TUM_ID}" --muttype fusion;
