a9f110369df5866567b34e0f2f2abd3c97abd010
ziching18 <zichingtan18@gmail.com>
2026-02-06 17:33:18 +0800

    update path

--- fusion_neoantigen/fn_extract_reads.sh
+++ fusion_neoantigen/fn_extract_reads.sh
@@ -2,11 +2,11 @@ TUM_ID=$1
 muttype=$2; ## should be fusion
 path=$(echo /stereoseq/all_samples/neoantigens/fusion);
 
-# touch $path/logs/$TUM_ID.$muttype.log.txt; ## create log file
-# linenumber=$(wc -l $path/logs/$TUM_ID.$muttype.log.txt | awk '{print $1}'); ## for resuming job; depracated
+touch $path/logs/$TUM_ID.$muttype.log.txt; ## create log file
+linenumber=$(wc -l $path/logs/$TUM_ID.$muttype.log.txt | awk '{print $1}'); ## for resuming job; depracated
 
 ## write number of transcripts in each bam into file
-echo neopeptide hla chr.pos epitope count > $path/counts/$TUM_ID.$muttype.counts.tsv;
+echo neopeptide hla chr.pos epitope count > $path/$TUM_ID/counts/$TUM_ID.$muttype.counts.tsv;
 while read line; do
     ## define new variant
     neopeptide=$(echo $line | awk '{print $4}');
@@ -20,11 +20,11 @@ while read line; do
 
     ## extract transcripts 
     samtools view -b /stereoseq/all_samples/bams/$TUM_ID/$TUM_ID.recal.RGA.bam $fullpos > \
-    $path/bams/$TUM_ID.$neopeptide.$hla.chr$fullpos.$epitope.$muttype.bam
+    $path/$TUM_ID/bams/$TUM_ID.$neopeptide.$hla.chr$fullpos.$epitope.$muttype.bam
     
     ## count number of transcripts in each corresponding bam and write into file
-    count=$(samtools view $path/bams/$TUM_ID.$neopeptide.$hla.chr$fullpos.$epitope.$muttype.bam | wc -l | awk '{print $1}');
-    echo $neopeptide $hla chr$fullpos $epitope $count >> $path/counts/$TUM_ID.$muttype.counts.tsv; # writing counts to file
+    count=$(samtools view $path/$TUM_ID/bams/$TUM_ID.$neopeptide.$hla.chr$fullpos.$epitope.$muttype.bam | wc -l | awk '{print $1}');
+    echo $neopeptide $hla chr$fullpos $epitope $count >> $path/$TUM_ID/counts/$TUM_ID.$muttype.counts.tsv; # writing counts to file
 done < <(grep $TUM_ID $path/stereo_fusion_neopeptides_sep.tsv) ## input defined by $muttype, default should be 'all' to extract all somatic.filtered1.funcotated.vcf variants
 
 touch $path/logs/$TUM_ID.$muttype.extract_reads.success;
