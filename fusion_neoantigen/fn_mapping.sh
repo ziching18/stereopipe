f8fb693ab5ebe39d0ec03426e5ffa2f79facd08f
ziching18 <zichingtan18@gmail.com>
2026-02-24 16:40:40 +0800

    fusion neoantigen mapping from fq

---
+++ fusion_neoantigen/fn_mapping.sh
@@ -0,0 +1,25 @@
+cd /stereoseq/
+
+ulimit -n 10240
+ulimit -v 33170449147
+NUMBA_CACHE_DIR=/stereoseq/test/cache
+
+dataDir=/stereoseq/SD507_SD693
+# outDir=/stereoseq/SD507_SD693
+refDir=/stereoseq/reference
+
+export SINGULARITY_BIND=$dataDir,$refDir
+
+bash /stereoseq/code/stereopipe/fusion_neoantigen/fn_saw_mapping.sh \
+    -sif ~/SAW_6.0.sif \
+    -genomeSize 5 \
+    -splitCount 1 \
+    -maskFile $dataDir/mask/SS200000873BR_D5.barcodeToPos.h5 \
+    -fq1 $dataDir/reads/E100052965_r1.fq.gz \
+    -fq2 $dataDir/reads/E100052965_r2.fq.gz \
+    -speciesName homo_sapiens \
+    -tissueType breast_tumour \
+    -refIndex $refDir/STAR_SJ100 \
+    -annotationFile $refDir/GRCh38.109.gtf \
+    -threads 16 \
+    -outDir $dataDir/result 
