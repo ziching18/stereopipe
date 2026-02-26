0f86c9de9bf629e72dbb3088fb16e0acf87fab10
ziching18 <zichingtan18@gmail.com>
2026-02-26 13:51:16 +0800

    increase number of threads to 24

--- fusion_neoantigen/fn_mapping.sh
+++ fusion_neoantigen/fn_mapping.sh
@@ -21,5 +21,5 @@ bash /stereoseq/code/stereopipe/fusion_neoantigen/fn_saw_mapping.sh \
     -tissueType breast_tumour \
     -refIndex $refDir/STAR_SJ100 \
     -annotationFile $refDir/GRCh38.109.gtf \
-    -threads 16 \
+    -threads 24 \
     -outDir $dataDir/result 
