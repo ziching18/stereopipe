c4f16f003315c11dc95eddcca9391e466d76507c
ziching18 <zichingtan18@gmail.com>
2026-04-09 09:55:29 +0800

    fix bcftool -i -e limitation

--- variant_processing/variant_filtering_deconstructSigs.sh
+++ variant_processing/variant_filtering_deconstructSigs.sh
@@ -9,10 +9,9 @@ bcftools norm -m -any /stereoseq/all_samples/vcf/${TUM_ID}/${TUM_ID}.somatic.fil
 bcftools view -f PASS -v snps step0.vcf -Oz -o step1.vcf
 
 # Step 2: quality filters
-bcftools view \
-  -i 'FORMAT/DP>10 && FORMAT/AD[1]>3 && FORMAT/AF[1:0]>=0.1 && INFO/TLOD>=6' \
-  -e 'INFO/STR=1' \
-  step1.vcf -Oz -o step2.vcf
+bcftools view -i 'FORMAT/DP>10 && FORMAT/AD[1]>3 && FORMAT/AF[1:0]>=0.1 && FORMAT/AF[1:0]<=0.8 && INFO/TLOD>=6' step1.vcf -Oz -o tmp.vcf
+
+bcftools view -e 'INFO/STR=1' tmp.vcf -Oz -o step2.vcf
 
 # Step 3: remove problematic regions
 bcftools view -r 1-22 step2.vcf -Oz -o step3.vcf
