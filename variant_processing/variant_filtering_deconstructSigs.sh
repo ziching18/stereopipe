0b70219454c1f648a747cdfd7f699b663877ab46
ziching18 <zichingtan18@gmail.com>
2026-04-09 10:14:48 +0800

    specify AD as [0:1]

--- variant_processing/variant_filtering_deconstructSigs.sh
+++ variant_processing/variant_filtering_deconstructSigs.sh
@@ -9,7 +9,7 @@ bcftools norm -m -any /stereoseq/all_samples/vcf/${TUM_ID}/${TUM_ID}.somatic.fil
 bcftools view -f PASS -v snps step0.vcf -Oz -o step1.vcf
 
 # Step 2: quality filters
-bcftools view -i 'FORMAT/DP>10 && FORMAT/AD[1]>3 && FORMAT/AF[1:0]>=0.1 && FORMAT/AF[1:0]<=0.8 && INFO/TLOD>=6' step1.vcf -Oz -o tmp.vcf
+bcftools view -i 'FORMAT/DP>10 && FORMAT/AD[0:1]>3 && FORMAT/AF[1:0]>=0.1 && FORMAT/AF[1:0]<=0.8 && INFO/TLOD>=6' step1.vcf -Oz -o tmp.vcf
 
 bcftools view -e 'INFO/STR=1' tmp.vcf -Oz -o step2.vcf
 
